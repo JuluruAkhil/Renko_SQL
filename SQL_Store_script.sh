1c1dd45fc2f49c85b96dfaa646e430deece0b924
eternalbliss <akhil.juluru@gmail.com>
2020-05-04 14:02:14 +0530

    Final

--- SQL_Store_script.sh
+++ SQL_Store_script.sh
@@ -5,5 +5,5 @@ PID=`cat /home/ubuntu/Renko_SQL/SQL_Store.pid`
 if ! ps -p $PID > /dev/null
 then
   rm /home/ubuntu/Renko_SQL/SQL_Store.pid
-  Python3 SQL_Store.py & echo $! >>/home/ubuntu/Renko_SQL/SQL_Store.pid
+  python3 SQL_Store.py & echo $! >>/home/ubuntu/Renko_SQL/SQL_Store.pid
 fi
--- sql_test_script.sh
+++ sql_test_script.sh
@@ -5,5 +5,5 @@ PID=`cat /home/ubuntu/Renko_SQL/sql_test.pid`
 if ! ps -p $PID > /dev/null
 then
   rm /home/ubuntu/Renko_SQL/sql_test.pid
-  Python3 sql_test.py & echo $! >>/home/ubuntu/Renko_SQL/sql_test.pid
+  python3 sql_test.py & echo $! >>/home/ubuntu/Renko_SQL/sql_test.pid
 fi
